bd8a2b2ecc765d27f3a526ad53b73a3896890206
Simone Parente <parentesimone.01@gmail.com>
2022-12-05 15:24:10 +0100

    Prova Utente

--- SQL/SQL.sql
+++ SQL/SQL.sql
@@ -1,36 +1,10 @@
 CREATE SCHEMA p;
 
-CREATE TABLE p.Negozio
+CREATE TABLE p.Utente
 (
-    ID_Negozio INT,
-    Nome VARCHAR(32),
-    CONSTRAINT Negozio_PK PRIMARY KEY(ID_Negozio)
+    username VARCHAR(64),      --PK
+    email VARCHAR(64),
+    password VARCHAR(64),
+
+    CONSTRAINT PK_Utente PRIMARY KEY (username)
 );
-
-CREATE TABLE p.Libro
-(
-    ISSN INT,
-    Titolo VARCHAR(32),
-    Autore VARCHAR(32),
-    Prezzo_copertina DOUBLE PRECISION,
-
-    CONSTRAINT Libro_PK PRIMARY KEY (ISSN)
-);
-
-CREATE TABLE p.StockLibri
-(
-    ISSN INT,
-    Negozio INT,
-    Quantita INT,
-    Prezzo_vendita DOUBLE PRECISION,
-
-    CONSTRAINT Stock_PK PRIMARY KEY (ISSN, Negozio),
-    CONSTRAINT Stock_ISSN FOREIGN KEY (ISSN) REFERENCES p.Libro(ISSN),
-    CONSTRAINT Stock_Negozio FOREIGN KEY(Negozio) REFERENCES p.Negozio(ID_Negozio)
-);
-
-CREATE TABLE p.StockArticoli
-(
-    DOI VARCHAR(64),
-    
-)
