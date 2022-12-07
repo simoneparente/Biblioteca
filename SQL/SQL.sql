0f843cd7c7dfd92be46f911cdc5a751d0408c588
davidesanti <d.santi@studenti.unina.it>
2022-12-07 16:27:28 +0100

    Login dao funzionante
    attenzione alla serie di break in LoginPage class

--- SQL/SQL.sql
+++ SQL/SQL.sql
@@ -8,3 +8,11 @@ CREATE TABLE p.Utente
 
     CONSTRAINT PK_Utente PRIMARY KEY (username)
 );
+
+INSERT INTO p.Utente (username, email, password)
+(
+    VALUES ('Simone', 'simone@gmail.it', 'Clemente'),
+           ('Mario', 'mario@gmail.it', 'Pinguino' ),
+           ('Davide', 'davide@gmail.it', 'Mammt' ),
+           ('Stefano', 'stefano@gmail.it', 'machiavass' )
+);
