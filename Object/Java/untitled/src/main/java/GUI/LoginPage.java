c0d4652397770583719c5eaab3f75a1dae4539f0
Simone Parente <parentesimone.01@gmail.com>
2023-01-24 17:08:35 +0100

    non funziona

--- Object/Java/untitled/src/main/java/GUI/LoginPage.java
+++ Object/Java/untitled/src/main/java/GUI/LoginPage.java
@@ -1,15 +1,14 @@
 package GUI;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class LoginPage {
     String frameIconPath="src/main/java/GUI/Images/icona.jpg";
-    String frameLogoPath="src/main/java/GUI/Images/logo.jpg";
+    String frameLogoPath="src/main/java/GUI/Images/provatesto.jpg";
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JPanel loginPanel;
-    private JLabel logo;
+    private JLabel labelImmagine;
 
     public LoginPage(){
         JFrame frame = new JFrame("Login");
@@ -19,13 +18,11 @@ public class LoginPage {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setIconImage(new ImageIcon(frameIconPath).getImage());
         frame.setVisible(true);
-        logo.setVisible(true);
-        frame.add(logo);
-
     }
 
     public static void main(String[] args) {
         new LoginPage();
     }
+
 
 }
