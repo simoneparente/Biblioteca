7e0f4137c5ed9b9d1b7fd1851acc14c8e721d4a6
davidesanti <d.santi@studenti.unina.it>
2023-01-25 12:27:54 +0100

    logloglog

--- Object/Java/Biblio/src/main/java/org/example/Login.java
+++ Object/Java/Biblio/src/main/java/org/example/Login.java
@@ -1,18 +1,21 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Login {
     private JFrame loginFrame;
     private JPanel rootPanel;
     private JPanel userPanel;
     private JPanel imagePanel;
-    private JButton annulla;
-    private JButton ok;
-    private JLabel immagine;
     private JTextField textField1;
     private JPasswordField passwordField1;
-    private JButton registerButton;
+    private JButton annullaButton;
+    private JButton okButton;
+    private JButton registerNowButton;
+    private JButton ok;
+    private JLabel immagine;
 
     Login(String nome){
         immagine.setIcon(new ImageIcon(""));
@@ -21,6 +24,11 @@ public class Login {
         loginFrame.setBounds(100,100, 720, 480);
 
 
+        ok.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
+            }
+        });
     }
 }
