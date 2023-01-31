9e7dd593b90143961fbb75c5ca2959f858579121
davidesanti <d.santi@studenti.unina.it>
2023-01-31 16:32:14 +0100

    Update ResultView.java

--- Object/Java/Bibliotech/src/main/java/org/Bibliotech/View/ResultView.java
+++ Object/Java/Bibliotech/src/main/java/org/Bibliotech/View/ResultView.java
@@ -2,9 +2,14 @@ package org.Bibliotech.View;
 
 import javax.swing.*;
 
-public class ResultView {
+public class ResultView extends View{
     private static ResultView instance = null;
     private JPanel rootPanel;
+
+    ResultView(String nome) {
+        super(nome);
+    }
+
     public ResultView getInstance() {
         if (instance == null) {
             instance = new ResultView();
