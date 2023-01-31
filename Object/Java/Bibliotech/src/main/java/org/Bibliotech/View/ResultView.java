ff1a6b1d86a118a110552bcc818d19dedb105a45
davidesanti <d.santi@studenti.unina.it>
2023-01-31 16:31:17 +0100

    chella singleton e sort

--- Object/Java/Bibliotech/src/main/java/org/Bibliotech/View/ResultView.java
+++ Object/Java/Bibliotech/src/main/java/org/Bibliotech/View/ResultView.java
@@ -3,5 +3,13 @@ package org.Bibliotech.View;
 import javax.swing.*;
 
 public class ResultView {
+    private static ResultView instance = null;
     private JPanel rootPanel;
+    public ResultView getInstance() {
+        if (instance == null) {
+            instance = new ResultView();
+        }
+        return instance;
+    }
 }
+
