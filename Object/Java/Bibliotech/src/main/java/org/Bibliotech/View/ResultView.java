fc2ca929e39a36c12696291e943f206e6ff5d870
Simone Parente <parentesimone.01@gmail.com>
2023-01-31 16:46:25 +0100

    prova static

--- Object/Java/Bibliotech/src/main/java/org/Bibliotech/View/ResultView.java
+++ Object/Java/Bibliotech/src/main/java/org/Bibliotech/View/ResultView.java
@@ -2,9 +2,17 @@ package org.Bibliotech.View;
 
 import javax.swing.*;
 
-public class ResultView {
+public class ResultView extends View{
+    private static final String nome="Result";
     private static ResultView instance = null;
     private JPanel rootPanel;
+    private JTable resultTable;
+    private JScrollPane resultScrollPane;
+
+    ResultView() {
+        super(nome);
+    }
+
     public ResultView getInstance() {
         if (instance == null) {
             instance = new ResultView();
