53ab0df3e1cc42ec8faa53c6c3aa1b92f5436c1b
Simone Parente <parentesimone.01@gmail.com>
2023-03-02 15:15:24 +0100

    sequence diagram 1

--- Object/Java/Bibliotech/src/main/java/org/Bibliotech/Main.java
+++ Object/Java/Bibliotech/src/main/java/org/Bibliotech/Main.java
@@ -4,9 +4,6 @@ import org.Bibliotech.Controller.Controller;
 
 public class Main {
     public static void main(String[] args) {
-        //AggiuntaView aggiuntaView = AggiuntaView.getInstance();
         Controller controller = Controller.getInstance();
-
-
     }
 }
